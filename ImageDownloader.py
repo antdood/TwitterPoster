5368b79ce1703172e454eb686caf72493ce7da74
kaeeeeede <kaedewrites1@gmail.com>
2022-12-09 01:59:41 +0800

    implemented image downloading from gdrive

---
+++ ImageDownloader.py
@@ -0,0 +1,98 @@
+from __future__ import print_function
+
+import os.path
+import io
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from dotenv import load_dotenv
+
+load_dotenv()
+SCOPES = [os.getenv('SCOPES')]
+folder_id = os.getenv('FOLDER_ID')
+
+def GetFiles(folder_id):
+    creds = None
+    
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        results = service.files().list(q=f"'{folder_id}' in parents",
+                                            spaces='drive',
+                                            fields='nextPageToken, '
+                                                   'files(id, name)',
+                                            pageToken=None).execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    except HttpError as error:
+
+        print(f'An error occurred: {error}')
+
+    return items
+
+def DownloadFile(file):
+    creds = None
+    
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        file_name = file['name']
+        file_id = file['id']
+
+        request = service.files().get_media(fileId=file_id)
+        file = io.FileIO(os.path.join('Downloads', f'{file_name}'), mode='wb')
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return
+
+if __name__ == '__main__':
+    items = GetFiles(folder_id)
+
+    for item in items:
+        DownloadFile(item)
