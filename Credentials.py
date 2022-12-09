a3171dca33a12d5f22497cc835c560d4410d36a7
kaeeeeede <kaedewrites1@gmail.com>
2022-12-09 22:05:44 +0800

    fixed minor issues

--- Credentials.py
+++ Credentials.py
@@ -3,30 +3,41 @@ import os.path
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Credential:
 	creds = None
+	SCOPES = [os.getenv('SCOPES')]
 
 	@classmethod
-	def InitCredentials(cls, SCOPES):
+	def InitCredentials(cls):
 		if os.path.exists('token.json'):
-			return Credentials.from_authorized_user_file('token.json', SCOPES)
+			return Credentials.from_authorized_user_file('token.json', cls.SCOPES)
 
 	@classmethod
-	def CheckCredentialsValidity(cls, SCOPES):
+	def IsCredsValid(cls):
 		if not cls.creds or not cls.creds.valid:
-			if cls.creds and cls.creds.expired and cls.creds.refresh_token:
-				cls.creds.refresh(Request())
-			else:
-				flow = InstalledAppFlow.from_client_secrets_file(
-					'credentials.json', SCOPES)
-				cls.creds = flow.run_local_server(port=0)
+			return False
+
+		return True
 
 	@classmethod
-	def GetCredentials(cls, SCOPES):
+	def RefreshCreds(cls):
 		if not cls.creds:
-			cls.creds = cls.InitCredentials(SCOPES)
+			cls.creds = cls.InitCredentials()
 
-		cls.CheckCredentialsValidity(SCOPES)
+		if cls.creds and cls.creds.expired and cls.creds.refresh_token:
+			cls.creds.refresh(Request())
+			
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', cls.SCOPES)
+			cls.creds = flow.run_local_server(port=0)
+
+	@classmethod
+	def GetCredentials(cls):
+		if not cls.IsCredsValid():
+			cls.RefreshCreds()
 
 		return cls.creds
--- ImageDownloader.py
+++ ImageDownloader.py
@@ -11,13 +11,11 @@ from googleapiclient.http import MediaIoBaseDownload
 from dotenv import load_dotenv
 
 load_dotenv()
-SCOPES = [os.getenv('SCOPES')]
 folder_id = os.getenv('FOLDER_ID')
 
-creds = Credential.GetCredentials(SCOPES)
-download_destinations = []
-
 def GetFiles(folder_id):
+    creds = Credential.GetCredentials()
+
     try:
         service = build('drive', 'v3', credentials=creds)
 
@@ -28,25 +26,25 @@ def GetFiles(folder_id):
                                             pageToken=None).execute()
         
         items = results.get('files', [])
-
-        if not items:
-            print('No files found.')
-            
-            return
         
-        print('Files:')
+        print('Files count:')
         
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
     
     except HttpError as error:
-        print(f'An error occurred: {error}')
+        raise error
 
     return items
 
-def DownloadFile(file):
+def DownloadFile(file, cleanupBeforeDownloading = True):
+    if cleanupBeforeDownloading:
+        PreDownloadCleanup()
+
     if not os.path.exists("Downloads"):
         os.makedirs('Downloads')
+
+    creds = Credential.GetCredentials()
 
     try:
         service = build('drive', 'v3', credentials=creds)
@@ -55,29 +53,28 @@ def DownloadFile(file):
         file_id = file['id']
 
         request = service.files().get_media(fileId=file_id)
-        file = io.FileIO(os.path.join('Downloads', f'{file_name}'), mode='wb')
-        downloader = MediaIoBaseDownload(file, request)
+        target_file = io.FileIO(os.path.join('Downloads', f'{file_name}'), mode='wb')
+        downloader = MediaIoBaseDownload(target_file, request)
         done = False
         
         while done is False:
             status, done = downloader.next_chunk()
             print(F'Download {int(status.progress() * 100)}.')
-            download_destinations.append(file_name)
+            download_destination = file_name
 
     except HttpError as error:
         print(F'An error occurred: {error}')
         file = None
 
-    return download_destinations
+    print(download_destination)
+
+    return download_destination
 
 def PreDownloadCleanup(folder = "Downloads"):
-    global download_destinations
-    download_destinations = []
     shutil.rmtree(folder, ignore_errors=True)
 
 if __name__ == '__main__':
     items = GetFiles(folder_id)
 
-    PreDownloadCleanup()
     for item in items:
         DownloadFile(item)
